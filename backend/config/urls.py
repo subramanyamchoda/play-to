27abe271c0a662cd8715c9fc121d316323131767
Choda Subramanyam <149325423+subramanyamchoda@users.noreply.github.com>
2026-04-26 19:24:34 +0530

    Update urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+
+def home(request):
+    return JsonResponse({"status": "ok", "message": "Backend is running"})
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+       path('', home), 
     path("api/", include("payouts.urls")),
 ]
